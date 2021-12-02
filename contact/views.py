26f2a45facdab863c8a3e584992f7fcbff8131b6
Ayen19 <rockatuna@gmail.com>
2021-12-02 17:27:29 -0600

    add view endpoints

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,23 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(res):
-    print("Hello")
+def read_contacts(res):
+    print("reading contacts...")
+    return render(res,"contact/index.html")
+
+def read_contact(res,id):
+    #find out other ways of string concats
+    print(f"reading contact...{id}")
+    return render(res,"contact/index.html")
+
+def create_contacts(res):
+    print("creating contacts...")
+    return render(res,"contact/index.html")
+
+def edit_contact(res,id):
+    print(f"editing contact...{id}")
+    return render(res,"contact/index.html")
+
+def delete_contact(res,id):
+    print(f"deleting contact...{id}")
     return render(res,"contact/index.html")
